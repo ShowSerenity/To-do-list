b7dfaa46bebc6658a5b400c1591550fa1b576c32
ShowSerenity <asanali.rymgali@gmail.com>
2023-10-16 14:40:02 +0600

    Добавьте файлы проекта.

---
+++ To-do-list/To-do-list.cpp
@@ -0,0 +1,11 @@
+﻿// To-do-list.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
